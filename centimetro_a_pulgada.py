47da9f6efab9694c7ac22bc8e149d7f74f87494f
carmen74 <cbravobartolon@gmail.com>
2016-09-06 02:32:49 +0200

    Create centimetro_a_pulgada.py

---
+++ centimetro_a_pulgada.py
@@ -0,0 +1,13 @@
+Escribe un programa que convierta de centímetros a pulgadas y viceversa.
+
+print("1. Centimetros a pulgadas")
+print("2. Pulgadas a centimetros")
+o = int(input("Selecciona una opcion (1 o 2): "))
+if o == 1:
+    c = float(input("Centimetros: "))
+    p = c * (1 / 2.54)
+    print("Pulgadas: ", p)
+elif o == 2:
+    p = float(input("Pulgadas: "))
+    c = p * 2.54
+    print("Centimetros: ", c)
